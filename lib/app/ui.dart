2c4a05b1222c5bc15db1c000ae398a948a4ccf81
sina.hash98 <sina.ares85@gmail.com>
2026-02-11 16:29:24 +0300

    adding more product

--- lib/app/ui.dart
+++ lib/app/ui.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
 class AppUI {
-  static const radius = 18.0;
-
-  static const padding = EdgeInsets.all(16);
-  static const cardPadding = EdgeInsets.all(14);
+  static const double radius = 18;
 
   static BorderRadius borderRadius([double r = radius]) =>
       BorderRadius.circular(r);
 
-  static const gridGap = 12.0;
+  static const EdgeInsets padding = EdgeInsets.all(16);
+  static const EdgeInsets cardPadding = EdgeInsets.all(14);
+
+  static const double gridGap = 12;
 }
